9c332af0f21941bbbcb213400ba749a19838cd12
benbroce <benbroce@byu.edu>
2024-02-28 22:28:27 -0700

    implement GameService and its unit tests

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -2,7 +2,12 @@ package service;
 
 import chess.ChessGame;
 import dataAccess.AuthDAO;
+import dataAccess.DataAccessException;
 import dataAccess.GameDAO;
+import model.GameData;
+import service.serviceExceptions.AlreadyTakenException;
+import service.serviceExceptions.BadRequestException;
+import service.serviceExceptions.UnauthorizedException;
 
 import java.util.ArrayList;
 
@@ -19,31 +24,69 @@ public class GameService {
     }
 
     /**
+     * Return a list of all the games in the database with their metadata
+     *
      * @param authToken the authentication token for the current session
      * @return a list of all current games
-     * @throws
+     * @throws UnauthorizedException if the authToken is invalid
      */
-    public ArrayList<ChessGame> listGames(String authToken) {
-        throw new RuntimeException("Not implemented");
+    public ArrayList<GameData> listGames(String authToken) throws UnauthorizedException {
+        if (!authDAO.verifyAuthToken(authToken)) {
+            throw new UnauthorizedException("bad auth token");
+        }
+        return gameDAO.listGames();
     }
 
     /**
+     * Create a new game in the database
+     *
      * @param authToken the authentication token for the current session
      * @param gameName  the desired display name of the game
      * @return the internal gameID
-     * @throws
+     * @throws UnauthorizedException if the authToken is invalid
      */
-    public int createGame(String authToken, String gameName) {
-        throw new RuntimeException("Not implemented");
+    public int createGame(String authToken, String gameName) throws UnauthorizedException {
+        if (!authDAO.verifyAuthToken(authToken)) {
+            throw new UnauthorizedException("bad auth token");
+        }
+        return gameDAO.createGame(gameName);
     }
 
     /**
+     * Join a game in the database
+     *
      * @param authToken   the authentication token for the current session
      * @param playerColor the desired player color to join as
      * @param gameID      the ID of the desired game to enter
-     * @throws
+     * @throws BadRequestException   if the requested game does not exist
+     * @throws UnauthorizedException if the authToken is invalid
+     * @throws AlreadyTakenException if the requested team position is already claimed
      */
-    public void joinGame(String authToken, ChessGame.TeamColor playerColor, int gameID) {
-        throw new RuntimeException("Not implemented");
+    public void joinGame(String authToken, ChessGame.TeamColor playerColor, int gameID) throws BadRequestException, UnauthorizedException, AlreadyTakenException {
+        if (!authDAO.verifyAuthToken(authToken)) {
+            throw new UnauthorizedException("bad auth token");
+        }
+        GameData gameData = gameDAO.getGame(gameID);
+        if (gameData == null) {
+            throw new BadRequestException("requested game does not exist");
+        }
+        if (playerColor == null) {
+            return; // observation mode
+        }
+        if (((playerColor == ChessGame.TeamColor.WHITE)
+                ? (gameData.whiteUsername() != null) : (gameData.blackUsername() != null))) {
+            throw new AlreadyTakenException("requested team already claimed");
+        }
+        String playerUsername = authDAO.getUsername(authToken);
+        String newWhiteUsername = (playerColor == ChessGame.TeamColor.WHITE) ? playerUsername : gameData.whiteUsername();
+        String newBlackUsername = (playerColor == ChessGame.TeamColor.BLACK) ? playerUsername : gameData.blackUsername();
+        gameData = new GameData(
+                gameData.gameID(), newWhiteUsername, newBlackUsername, gameData.gameName(), gameData.game());
+        // convert any DataAccessException -> BadRequestException
+        try {
+            gameDAO.updateGame(gameID, gameData);
+        } catch (DataAccessException e) {
+            throw new BadRequestException("game does not exist");
+        }
     }
 }
--- server/src/test/java/serviceTests/GameServiceTest.java
+++ server/src/test/java/serviceTests/GameServiceTest.java
@@ -1,9 +1,17 @@
 package serviceTests;
 
+import chess.ChessGame;
 import dataAccess.*;
+import model.GameData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import service.GameService;
+import service.serviceExceptions.AlreadyTakenException;
+import service.serviceExceptions.BadRequestException;
+import service.serviceExceptions.UnauthorizedException;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,25 +29,123 @@ class GameServiceTest {
 
     @Test
     public void listGamesTestPositive() {
+        // set pre-state
+        int gameID0 = gameDAO.createGame("game0");
+        int gameID1 = gameDAO.createGame("game1");
+        String authToken = authDAO.createAuth("testUser");
+        // confirm pre-state
+        assertNotNull(gameDAO.getGame(gameID0));
+        assertNotNull(gameDAO.getGame(gameID1));
+        assertEquals(authDAO.getUsername(authToken), "testUser");
+        // perform listGames
+        ArrayList<GameData> gameList = new ArrayList<>();
+        try {
+            gameList = gameService.listGames(authToken);
+        } catch (UnauthorizedException e) {
+            fail("bad auth token");
+        }
+        // compare post-state
+        assertEquals(gameList.get(0).gameName(), "game0");
+        assertEquals(gameList.get(1).gameName(), "game1");
     }
 
     @Test
     public void listGamesTestNegative() {
+        // set pre-state
+        int gameID1 = gameDAO.createGame("game0");
+        int gameID2 = gameDAO.createGame("game1");
+        String authToken = authDAO.createAuth("testUser");
+        // confirm pre-state
+        assertNotNull(gameDAO.getGame(gameID1));
+        assertNotNull(gameDAO.getGame(gameID2));
+        assertEquals(authDAO.getUsername(authToken), "testUser");
+        // perform listGames, compare post-state (thrown credentialing exception)
+        assertThrows(UnauthorizedException.class, (() -> gameService.listGames("wrongAuthToken")));
     }
 
     @Test
     public void createGameTestPositive() {
+        // set pre-state
+        String authToken = authDAO.createAuth("testUser");
+        // confirm pre-state
+        assertEquals(authDAO.getUsername(authToken), "testUser");
+        // perform createGame
+        int gameID = -1;
+        try {
+            gameID = gameService.createGame(authToken, "testGame");
+        } catch (UnauthorizedException e) {
+            fail("Unauthorized to create game");
+        }
+        // compare post-state
+        assertNotNull(gameDAO.getGame(gameID));
     }
 
     @Test
     public void createGameTestNegative() {
+        // set pre-state
+        String authToken = authDAO.createAuth("testUser");
+        // confirm pre-state
+        assertEquals(authDAO.getUsername(authToken), "testUser");
+        // perform createGame (invalid, bad authentication)
+        assertThrows(UnauthorizedException.class,
+                (() -> gameService.createGame("wrongAuthToken", "testGame")));
+        // compare post-state
+        assertEquals(gameDAO.listGames().size(), 0);
     }
 
     @Test
     public void joinGameTestPositive() {
+        // set pre-state
+        int gameID = gameDAO.createGame("game0");
+        String authToken = authDAO.createAuth("testUser");
+        // confirm pre-state
+        assertNotNull(gameDAO.getGame(gameID));
+        assertNull(gameDAO.getGame(gameID).whiteUsername());
+        assertEquals(authDAO.getUsername(authToken), "testUser");
+        // perform joinGame
+        try {
+            gameService.joinGame(authToken, ChessGame.TeamColor.WHITE, gameID);
+        } catch (BadRequestException e) {
+            fail("bad request");
+        } catch (UnauthorizedException e) {
+            fail("unauthorized");
+        } catch (AlreadyTakenException e) {
+            fail("position already taken");
+        }
+        // compare post-state
+        assertEquals(gameDAO.getGame(gameID).whiteUsername(), "testUser");
     }
 
     @Test
     public void joinGameTestNegative() {
+        // set pre-state
+        int gameID = gameDAO.createGame("game0");
+        String authToken = authDAO.createAuth("testUser");
+        String opponentAuthToken = authDAO.createAuth("opponent");
+        // confirm pre-state
+        assertNotNull(gameDAO.getGame(gameID));
+        assertNull(gameDAO.getGame(gameID).whiteUsername());
+        assertEquals(authDAO.getUsername(authToken), "testUser");
+        assertEquals(authDAO.getUsername(opponentAuthToken), "opponent");
+        // perform invalid joinGame (game doesn't exist), confirm throw
+        assertThrows(BadRequestException.class,
+                (() -> gameService.joinGame(authToken, ChessGame.TeamColor.WHITE, -1)));
+        // perform invalid joinGame (credentials incorrect), confirm throw
+        assertThrows(UnauthorizedException.class,
+                (() -> gameService.joinGame("wrongAuthToken", ChessGame.TeamColor.BLACK, gameID)));
+        // perform invalid joinGame (position already taken by opponent), confirm throw
+        try {
+            gameService.joinGame(opponentAuthToken, ChessGame.TeamColor.BLACK, gameID);
+        } catch (BadRequestException e) {
+            fail("opponent bad request");
+        } catch (UnauthorizedException e) {
+            fail("opponent unauthorized");
+        } catch (AlreadyTakenException e) {
+            fail("opponent position already taken");
+        }
+        assertThrows(AlreadyTakenException.class,
+                (() -> gameService.joinGame(authToken, ChessGame.TeamColor.BLACK, gameID)));
+        // compare post-state
+        assertEquals(gameDAO.getGame(gameID).blackUsername(), "opponent");
     }
 }
