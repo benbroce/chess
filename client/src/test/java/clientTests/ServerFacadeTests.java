1005f4ae293f05457e080299b77387b1ce4447be
benbroce <benbroce@byu.edu>
2024-03-22 20:38:48 -0600

    Complete ServerFacadeTests

--- client/src/test/java/clientTests/ServerFacadeTests.java
+++ client/src/test/java/clientTests/ServerFacadeTests.java
@@ -5,111 +5,158 @@ import clientAPI.ServerFacade;
 import org.junit.jupiter.api.*;
 import server.Server;
 
-public class ServerFacadeTests {
+import static org.junit.jupiter.api.Assertions.*;
 
+public class ServerFacadeTests {
     private static Server server;
     private static ServerFacade serverFacade;
 
     @BeforeAll
-    public static void init() throws ResponseException {
+    public static void init() {
         // set up server
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
         // set up client server facade
         serverFacade = new ServerFacade("http://localhost:" + port);
+    }
+
+    @BeforeEach
+    @AfterEach
+    public void cleanup() throws ResponseException {
         serverFacade.clearApp();
     }
 
     @AfterAll
-    static void stopServer() throws ResponseException {
-        serverFacade.clearApp();
+    static void stopServer() {
         server.stop();
     }
 
-    @Test
-    public void isLoggedInTestPositive() {
-        Assertions.assertTrue(true);
-    }
-
-    @Test
-    public void isLoggedInTestNegative() {
-        Assertions.assertTrue(true);
-    }
 
     // Admin Methods //////////////////////////////////////////////////////////////////////////////
 
     @Test
-    public void clearAppTestPositive() {
-        Assertions.assertTrue(true);
+    public void clearAppTestPositive() throws ResponseException {
+        serverFacade.register("testUser", "testPass", "testEmail");
+        serverFacade.createGame("testGame");
+        assertTrue(serverFacade.isLoggedIn());
+        assertFalse(serverFacade.listGames().isEmpty());
+        serverFacade.clearApp();
+        assertFalse(serverFacade.isLoggedIn());
+        assertThrows(ResponseException.class, (() -> serverFacade.listGames()));
     }
 
     @Test
-    public void clearAppTestNegative() {
-        Assertions.assertTrue(true);
+    public void clearAppTestNegative() throws ResponseException {
+        serverFacade.clearApp();
+        assertFalse(serverFacade.isLoggedIn());
+        assertThrows(ResponseException.class, (() -> serverFacade.listGames()));
     }
 
     // User Methods ///////////////////////////////////////////////////////////////////////////////
 
     @Test
-    public void registerTestPositive() {
-        Assertions.assertTrue(true);
+    public void isLoggedInTestPositive() throws ResponseException {
+        serverFacade.register("testUser", "testPass", "testEmail");
+        assertTrue(serverFacade.isLoggedIn());
+    }
+
+    @Test
+    public void isLoggedInTestNegative() {
+        assertFalse(serverFacade.isLoggedIn());
+    }
+
+    @Test
+    public void registerTestPositive() throws ResponseException {
+        assertFalse(serverFacade.isLoggedIn());
+        serverFacade.register("testUser", "testPass", "testEmail");
+        assertTrue(serverFacade.isLoggedIn());
+        assertDoesNotThrow(() -> serverFacade.logout());
     }
 
     @Test
     public void registerTestNegative() {
-        Assertions.assertTrue(true);
+        assertThrows(ResponseException.class,
+                (() -> serverFacade.register(null, "testPass", "testEmail")));
     }
 
     @Test
-    public void loginTestPositive() {
-        Assertions.assertTrue(true);
+    public void loginTestPositive() throws ResponseException {
+        serverFacade.register("testUser", "testPass", "testEmail");
+        serverFacade.logout();
+        assertFalse(serverFacade.isLoggedIn());
+        serverFacade.login("testUser", "testPass");
+        assertTrue(serverFacade.isLoggedIn());
     }
 
     @Test
-    public void loginTestNegative() {
-        Assertions.assertTrue(true);
+    public void loginTestNegative() throws ResponseException {
+        serverFacade.register("testUser", "testPass", "testEmail");
+        serverFacade.logout();
+        assertFalse(serverFacade.isLoggedIn());
+        assertThrows(ResponseException.class, (() -> serverFacade.login("testUser", "wrongPass")));
     }
 
     @Test
-    public void logoutTestPositive() {
-        Assertions.assertTrue(true);
+    public void logoutTestPositive() throws ResponseException {
+        serverFacade.register("testUser", "testPass", "testEmail");
+        assertDoesNotThrow(() -> serverFacade.listGames());
+        serverFacade.logout();
+        assertThrows(ResponseException.class, (() -> serverFacade.listGames()));
     }
 
     @Test
     public void logoutTestNegative() {
-        Assertions.assertTrue(true);
+        assertThrows(ResponseException.class, (() -> serverFacade.logout()));
     }
 
     // Game Methods ///////////////////////////////////////////////////////////////////////////////
 
     @Test
-    public void listGamesTestPositive() {
-        Assertions.assertTrue(true);
+    public void listGamesTestPositive() throws ResponseException {
+        serverFacade.register("testUser", "testPass", "testEmail");
+        serverFacade.createGame("testGame");
+        assertEquals(serverFacade.listGames().size(), 1);
+        serverFacade.createGame("testGame");
+        assertEquals(serverFacade.listGames().size(), 2);
     }
 
     @Test
     public void listGamesTestNegative() {
-        Assertions.assertTrue(true);
+        assertThrows(ResponseException.class, (() -> serverFacade.listGames()));
     }
 
     @Test
-    public void createGameTestPositive() {
-        Assertions.assertTrue(true);
+    public void createGameTestPositive() throws ResponseException {
+        serverFacade.register("testUser", "testPass", "testEmail");
+        int gameID = serverFacade.createGame("testGame");
+        assertEquals(gameID, serverFacade.listGames().getFirst().gameID());
     }
 
     @Test
-    public void createGameTestNegative() {
-        Assertions.assertTrue(true);
+    public void createGameTestNegative() throws ResponseException {
+        assertThrows(ResponseException.class, (() -> serverFacade.createGame("testGame")));
+        serverFacade.register("testUser", "testPass", "testEmail");
+        assertThrows(ResponseException.class, (() -> serverFacade.createGame(null)));
     }
 
     @Test
-    public void joinGameTestPositive() {
-        Assertions.assertTrue(true);
+    public void joinGameTestPositive() throws ResponseException {
+        serverFacade.register("testUser", "testPass", "testEmail");
+        int gameID = serverFacade.createGame("testGame");
+        serverFacade.joinGame("WHITE", gameID);
+        assertEquals(serverFacade.listGames().getFirst().whiteUsername(), "testUser");
     }
 
     @Test
-    public void joinGameTestNegative() {
-        Assertions.assertTrue(true);
+    public void joinGameTestNegative() throws ResponseException {
+        // not authorized
+        assertThrows(ResponseException.class, (() -> serverFacade.joinGame("WHITE", 0)));
+        // already taken
+        serverFacade.register("otherUser", "testPass", "testEmail");
+        int gameID = serverFacade.createGame("testGame");
+        serverFacade.joinGame("WHITE", gameID);
+        serverFacade.register("testUser", "testPass", "testEmail");
+        assertThrows(ResponseException.class, (() -> serverFacade.joinGame("WHITE", gameID)));
     }
 }
